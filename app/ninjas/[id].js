f6ef89174c98ae71cfbb2130693c9f90ed1222b6
Callum Crichton <callum.dev@proton.me>
2026-05-03 22:23:38 +0100

    work in progress before codemod

--- app/ninjas/[id].js
+++ app/ninjas/[id].js
@@ -1,5 +1,5 @@
 export const getStaticPaths = async () => {
-	const res = await fetch("http://localhost:3000/api/ninjas");
+	const res = await fetch("http://jsonplaceholder.typicode.com/users");
 	const data = await res.json();
 
 	const paths = data.map((ninja) => {
@@ -13,10 +13,25 @@ export const getStaticPaths = async () => {
 	};
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+	const id = context.params.id;
+	const res = await fetch("http://jsonplaceholder.typicode.com/users/" + id);
+	const data = await res.json();
+
+	return {
+		props: {
+			ninja: data,
+		},
+	};
+};
+
+const Details = ({ ninja }) => {
 	return (
 		<div>
-			<h3>Details Page</h3>
+			<h1>{ninja.name}</h1>
+			<p>{ninja.email}</p>
+			<p>{ninja.website}</p>
+			<p>{ninja.address.city}</p>
 		</div>
 	);
 };
