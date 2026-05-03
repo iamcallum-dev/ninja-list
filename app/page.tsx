806e1f79400e84c5db69ac53c81c90e7c3c31611
Callum Crichton <callum.dev@proton.me>
2026-05-03 18:07:19 +0100

    added components footer and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,15 @@
 import Image from "next/image";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Home() {
 	return (
 		<div>
+			<Navbar />
 			<h1>Homepage</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia culpa
-				omnis repudiandae accusantium soluta iure, molestias reprehenderit
-				praesentium quis nemo ex ea amet optio vero. Accusamus a nihil ipsum
-				vel!
-			</p>
-			<p>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur
-				enim praesentium nulla a quis iste nisi eveniet unde. Ea exercitationem
-				animi, dolor voluptatum culpa cupiditate. Sequi impedit consequatur
-				consequuntur neque.
-			</p>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+			<Footer />
 		</div>
 	);
 }
