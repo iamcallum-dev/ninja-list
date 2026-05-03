cb504862d1dc6f725e2bff6ae50782d59ce48248
Callum Crichton <callum.dev@proton.me>
2026-05-03 22:03:48 +0100

    fetching data

--- app/ninjas/page.tsx
+++ app/ninjas/page.tsx
@@ -1,5 +1,27 @@
-const Ninjas = () => {
-	return <p>All Ninjas!</p>;
+import styles from "../Ninjas.module.css";
+
+interface Ninja {
+	id: number;
+	name: string;
+	email: string;
+}
+
+const Ninjas = async () => {
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const ninjas: Ninja[] = await res.json();
+
+	return (
+		<div>
+			<h1>All Ninjas</h1>
+			{ninjas.map((ninja) => (
+				<div key={ninja.id}>
+					<a className={styles.single}>
+						<h3>{ninja.name}</h3>
+					</a>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Ninjas;
