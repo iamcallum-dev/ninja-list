831cfd320b7a43f47b5470f3c39bbefcaa059d9e
Callum Crichton <callum.dev@proton.me>
2026-05-03 19:46:35 +0100

    added redirectering users functionality

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,15 @@
 import Link from "next/link";
+import { use, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const NotFound = () => {
+	const router = useRouter();
+	useEffect(() => {
+		setTimeout(() => {
+			router.push("/");
+		}, 3000);
+	}, []);
+
 	return (
 		<div className='not-found'>
 			<h1>Ooops...</h1>
